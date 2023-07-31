88c86ba10ecbf67544d3e44c74afabe62c2593cf
starrk12 <lrobertkipkoech@gmail.com>
2023-07-31 21:54:01 +0200

    fixed input bug

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let win;
 function introduction() {
     
     const message = document.createElement('h2')
-
+    
     message.textContent = 'The game is the Classic Rock, Paper and Scissors. You simply have to pick one of the choices given below and the computer will also pick one;';
     message.style.color = 'pink'
 
@@ -17,6 +17,8 @@ function introduction() {
 
 }
 function playRound() {
+    let userChoice;
+
     const container = document.createElement('div')
     const rock = document.createElement('button');
     const paper = document.createElement('button');
@@ -26,6 +28,12 @@ function playRound() {
     rock.textContent = 'Rock';
     rock.setAttribute('style', 'color: red');
     rock.addEventListener('click', () => {
+        const userPoint = document.createElement('p')
+
+        userPoint.textContent = 'You picked Rock'
+        userPoint.style.color = 'brown'
+
+        document.body.append(userPoint)
         let userChoice = choice[0]
         getWinner(choice[0])
 
@@ -34,6 +42,12 @@ function playRound() {
     paper.textContent = 'Paper';
     paper.setAttribute('stye', 'color: white', 'backgroundColor: black');
     paper.addEventListener('click', () => {
+        const userPoint = document.createElement('p')
+
+        userPoint.textContent = 'You picked Paper'
+        userPoint.style.color = 'brown'
+
+        document.body.append(userPoint)
         let userChoice = choice[1]
         getWinner(choice[1])
         
@@ -42,6 +56,12 @@ function playRound() {
     scissors.textContent = 'Scissors';
     scissors.setAttribute('style', 'color: grey', 'backgroundColor: yellow');
     scissors.addEventListener('click', () => {
+        const userPoint = document.createElement('p')
+
+        userPoint.textContent = 'You picked Scissors'
+        userPoint.style.color = 'brown'
+
+        document.body.append(userPoint)
         let userChoice = choice[2]
         getWinner(choice[2])
     })
@@ -90,55 +110,58 @@ function getWinner(userChoice) {
     getComputerChoice()
     
 
-    if (userChoice == computerChoice){
+    if (userChoice === computerChoice){
         const draw = document.createElement('p')
         draw.textContent = 'Please have another choice. You choose the same!'
 
         document.body.append(draw)
         
-        introduction()
+        playRound()
     }
-    else if (userChoice == choice[0] && computerChoice == choice[1] ){
+    else if (userChoice === choice[0] && computerChoice === choice[1] ){
         const loose = document.createElement('p')
 
-        loose.textContent = 'Paper covers rock. You loose!'
+        loose.textContent = 'Paper covers rock. You loose! Computer Wins!'
         document.body.append(loose)
-        replayGame()
+
+        looser()        
     }
     else if (userChoice == choice[1] && computerChoice == choice[2]){
         const loose = document.createElement('p')
 
         loose.textContent = 'Scissors cut paper. You loose!'
         document.body.append(loose)
-        replayGame()
+        
+        looser()
     }
     else if (userChoice == choice[2] && computerChoice == choice[0]){
         const loose = document.createElement('p')
 
         loose.textContent = 'You loose! Rock crush scissors'
         document.body.append(loose)
-        replayGame()
+        
+        looser()
     }
     else if (userChoice == choice[0] && computerChoice == choice[2]){
         const win = document.createElement('p')
 
         win.textContent = 'You win, Rock crush scissors!'
         document.body.append(win)
-        replayGame()
+        
     }
     else if (userChoice == choice[1] && computerChoice == choice[0]){
         const win = document.createElement('p')
 
         win.textContent = 'You win, Paper covers Rock!'
         document.body.append(win)
-        replayGame()
+        
     }
     else if (userChoice == choice[2] && computerChoice == choice[1]){
         const win = document.createElement('p')
 
         win.textContent = 'You win!, Scissors cuts paper!'
         document.body.append(win)
-        replayGame()
+        
     }
     
 }
@@ -148,11 +171,21 @@ function replayGame() {
     replay.textContent = 'REPLAY'
     replay.style.color = 'red'
 
-    replay.addEventListener('click', introduction)
+    replay.addEventListener('click', playRound())
     document.body.append(replay)
 
 }
+function looser() {
+    const message = document.createElement('p')
+    const start = document.createElement('button')
 
+    message.textContent = 'You lost this round. You are welcome to play again. You can press START'
+    start.textContent = 'start'
+    start.addEventListener('click', introduction)
+    start.addEventListener('click', introduction)
+    
+
+}
 
 const start = document.getElementById('start')
 
